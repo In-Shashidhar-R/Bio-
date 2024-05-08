c448ceb318aaa94b43d872be1b3541fa98887347
Shashidhar R <127376016+In-Shashidhar-R@users.noreply.github.com>
2024-05-08 05:49:38 +0000

    hi

--- IBS - II/Kmers.py
+++
@@ -1,8 +0,0 @@
-input = "ATAAGGCTACATGCG"
-k_v=[3,4,5]
-for k in k_v:
-    seq=[]
-    for i in range(len(input)-k+1):
-        seq.append(input[i:i+k])
-    print("Sequence of",k,"-mers:")
-    print(sorted(seq))
